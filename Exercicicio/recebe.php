9a24bd48ad64ecdcbaebc2434da588cb6c03904f
enzo <enzo.olimachado@gmail.com>
2026-06-10 10:48:50 -0300

    hoje configuramos banco de dados

---
+++ Exercicicio/recebe.php
@@ -0,0 +1,15 @@
+<?php 
+
+$email = $_POST['email'];
+echo "$email <br>";
+$reemail = $_POST['reemail'];
+echo "$reemail <br>";
+$senha = $_POST['senha'];
+echo "$senha <br>";
+$email = $_POST['resenha'];
+echo "$resenha <br>";
+
+
+?>
+
+
