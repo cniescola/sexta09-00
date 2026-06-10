75c529bffdea8bef8729b18bc7089243208c0403
mabel <isabelcesario17@gmail.com>
2026-06-10 10:48:07 -0300

    aprendemos a coletar as informações através do php e começo do banco de dados

---
+++ exercicios/recebe.php
@@ -0,0 +1,13 @@
+<?php
+$email = $_POST["email"];
+echo "$email <br>";
+
+$Remail = $_POST["Remail"];
+echo "$Remail <br>";
+
+$senha = $_POST["senha"];
+echo "$senha <br>";
+
+$Rsenha = $_POST["Rsenha"];
+echo "$Rsenha <br>";
+?>
