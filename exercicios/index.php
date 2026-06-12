550edeca3b08085bfe44702bbcc7c7dbc9a6ef4e
vitor brenno <cniescola85@gmail.com>
2026-06-12 10:53:54 -0300

    começamos a fazer um site de cadastro de protudos para revisar bootstrap e aprender a usar banco de dados

---
+++ exercicios/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+  <body>
+    
+    <header class="container-fluid p-3 d-flex justify-content-between align-itens" style="background-color:#fff159;">
+        <a href="#"><img src="logo.png" class="logo" style="height:3rem"></a>
+        <div class="btn-hover">Cadastro Produto</div>
+        <div class="row">
+            <div class="col"><i class="bi bi-person-circle"></i>Login</div>
+            <div class="col">Contato</div>
+        </div>
+    </header>
+
+    <main class="container-fluid p-0">
+        <section class="d-flex flex-column">
+            <div class="superior w-100 p-5" style="background-color: #f5f5f5">
+                <div class="row">
+                    <div class="col">
+                        <div class="btn-hover">< Anterior</div>
+                        <div class="escrita">Para anunciar mais rápido, procure seu produto em nosso catalógo</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="inferior w-100" style="background-color: #ededed">
+            </div>
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
