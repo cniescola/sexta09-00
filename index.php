e882b9c3a0b99a28442b0a09a26962c1a43e9a95
vitor brenno <cniescola85@gmail.com>
2026-06-12 09:26:51 -0300

    header mercado livre

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  </head>
+  <body>
+    
+    <header class="container-fluid p-3 d-flex justify-content-between align-items-center" style="background-color:#fff159;">
+        <a href="#"><img src="logo.png" class="logo" style="height:3rem;"></a>
+        <div class="row">
+            <div class="col flex-row d-flex"><i class="bi bi-person-circle"></i> Login</div>
+            <div class="col">Contato</div>
+        </div>
+    </header>
+
+    <main class="container-fluid">
+        <section class="container">
+
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
