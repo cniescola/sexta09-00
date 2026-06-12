5cb1e5c8a961e89cb82d5e335ed13a0dd4e93a81
vitor brenno <cniescola85@gmail.com>
2026-06-12 10:59:45 -0300

    superior inferior

--- index.php
+++ index.php
@@ -6,23 +6,34 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     
-    <header class="container-fluid p-3 d-flex justify-content-between align-items-center" style="background-color:#fff159;">
+    <header class=" p-3 d-flex justify-content-between align-items-center" style="background-color:#fff159;">
         <a href="#"><img src="logo.png" class="logo" style="height:3rem;"></a>
+        <div class="btn-hover">Cadastra Produto</div>
         <div class="row">
             <div class="col flex-row d-flex"><i class="bi bi-person-circle"></i> Login</div>
             <div class="col">Contato</div>
         </div>
     </header>
 
-    <main class="container-fluid">
-        <section class="container">
+    <main class="container-fluid p-0">
+        <section class="d-flex flex-column">
+            <div class="superior w-100" style="background-color:#f5f5f5">
 
+            </div>
+            <div class="inferior w-100" style="background-color: #ededed;"></div>
         </section>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
+
+    <script>
+
+    </script>
   </body>
 </html>
