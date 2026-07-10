1fa57802377621fd0e23928ccb9c71e6dcb43727
Enzo <enzo.olimachado@gmail.com>
2026-07-10 11:19:20 -0300

    Hoje Fizemos o sit do mercado livre(novidade)

--- index.php
+++ index.php
@@ -1,62 +1,38 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mercado Preso</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-
-  <header class="container-fluid d-flex justify-content-between align-items-center p-1" style="background-color:#fff159">
-    <a href="#"><img src="logo.png" id="imgfoda" class="logo"></a>
-    <div class="btn-hover">Cadrasto Podruto</div>
-    <div class="row">
-      <div class="col flex-row d-flex"><i class="bi bi-person-circle"></i>Golin</div>
-      <div class="col">Contotas</div>
-    </div>
-
-  </header>
-  <main class="container-fluid p-0">
-    <section class="d-flex flex-column">
-      <div class="superior" style="background-color:#f5f5f5">
-        <div class="container mt-4"><a href="#" class="text-decoration-none">
-            < Anteio</a>
-        </div>
-        <div class="container mt-5 text-secondary">
-          <h6> Epata 2 de 2</h6>
-        </div>
-        <div class="container mt-1">
-          <h1> Para Conculi,</h1>
-        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <header class="ml-header">
         <div class="container">
-          <h1> Vamus defini as condiçoes foda</h1>
-        </div>
-        <div class="container mt-5 card">
-          <div class="row">
-            <div class="col">
-              <h6 class="mt-4">Quau e o Peçro?</h6>
-              <div id="caixavermelhahorrorosa" class="row">
-                <div class="col-4"><input class="" id="InputFoda" placeholder="R$"></div>
-              </div>
-            </div>
-            <div class="col d-flex align-items-end">
-              <input id="botao2" type="button" class="btn btn-success ms-5 mb-4" value="Cancela">
-              <input id="botao" type="button" class="btn btn-success ms-5 mb-4" value="Confirima">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="inferior" style="background-color:#ededed"></div>
+            <div class="d-flex justify-content-between align-items-center">
+                <a href="index.php" id="logo" class="d-flex">
+                    <img src="logo.png" alt="logo mercado livre"></img>
+                </a>
+                <div class="d-flex gap-3 align-items-center">
+                    <div id="CirculoAviadado" class="rounded-circle bg-secondary justify-content-center align-items-center text-white">RG</div>
+                    <span id="abrasuamente" class="ml-nav-link fw-bold">Robocop Ga...</span>
+                    <div class="d-flex align-items-center">
+                        <span id="abrasuamente" class="ml-nav-link">Contato</span>
+                        <div class="d-flex align-items-center">
+                            <a href="produto.php" class="btn btn-outline-dark" style="font-size: 0.75rem">Meus Produtos</a>
+                        </div>
+                    </div>
 
-    </section>
+                </div>
+            </div>
+    </header>
+    <main id="SeusCabeloEdaora" class="py-5">
 
-  </main>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </main>
 </body>
 
 </html>
