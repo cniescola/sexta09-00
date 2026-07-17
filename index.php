ecdea6cce1c07c9d6f5e9f041a5c38b93e6e4528
enzo foda <enzo.olimachado@gmail.com>
2026-07-17 11:27:19 -0300

    Fui Vendido no mercado negro

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="pt-br">
 
 <head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,9 +31,46 @@
                 </div>
             </div>
     </header>
-    <main id="SeusCabeloEdaora" class="py-5">
-
+    <main id="SeusCabeloEdaora" class="container-fluid py-5">
+        <section class="container">
+            <div class="text-center mb-5">
+                <h1 class="ml-title mb-3">Ola, Antes de mais nada<br>vamos exterminar sua existencia?<h1>
+            </div>
+            <div class="row justify-content-center g-4">
+                <div class="col-2">
+                    <a href="#" class="category-card text-decoration-none"><i class="bi bi-archive"></i><br>Podrutos</a>
+                </div>
+                <div class="col-2">
+                    <a href="#" class="category-card text-decoration-none"><i class="bi bi-truck"></i><br>Vehilucos</a>
+                </div>
+                <div class="col-2">
+                    <a href="#" class="category-card text-decoration-none"><i class="bi bi-house"></i><br>Imoveis</a>
+                </div>
+                <div class="col-2">
+                    <a href="#" class="category-card text-decoration-none"><i class="bi bi-tropical-storm"></i><br>Sevriços</a>
+                </div>
+                <div class="row justify-content-center text-center mt-5">
+                    <span id="idfoda"><i class="bi bi-window-desktop"></i> placeholder de extremo sucesso <a href="#" class="text-decoration-none">Placeholder Azul</a></span>
+                </div>
+            </div>
+        </section>
     </main>
+    <footer class="container-fluid">
+        <div class="d-flex justify-content-center align-items-center ">
+            <span class="mt-4" id="idfoda">Placeholder Fodalistico de alto respeito <a href="#" class="text-decoration-none">Placeholder Azul2</a></span>
+        </div>
+        <div class="mt-3 d-flex flex-wrap justify-content-center gap-3 mb-3 text-center">
+            <a id="idfoda" href="#" class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            <a id="idfoda" href="#"class="text-decoration-none">Trabalhe Com o DJABO</a>
+            
+        </div>
+        </div>
+    </footer>
 </body>
 
 </html>
